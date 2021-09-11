7cd46d713072525d14506326237987af5dfbf042
Eric Nantes <ericnantes18@gmail.com>
2021-09-11 14:22:53 -0400

    Adding home anchor in logo image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import { Container, Wrapper, StyledNavLink } from './styles';
 
 import Logo from '../../assets/lumis.svg';
+import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
     <Container>
       <Wrapper>
-        <img src={Logo} alt="Lumis" />
+        <Link to="/">
+          <img src={Logo} alt="Lumis" />
+        </Link>
         <nav>
           <StyledNavLink exact to="/" activeClassName="active-link">Home</StyledNavLink>
           <StyledNavLink to="/contact" activeClassName="active-link">Contato</StyledNavLink>
